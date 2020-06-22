12019367ae39c236ed7c559d7bb03e5fcdb97beb
Seongjun-Moon <moonsj1128@gmail.com>
2020-06-22 10:22:27 +0900

    간단한 N의 약수

---
+++ SWEA/1933.py
@@ -0,0 +1,13 @@
+"""
+1933. 간단한 N의 약수
+"""
+
+import sys
+sys.stdin = open('1933_input.txt')
+
+N = int(input())
+print(N)
+answer =[]
+for i in range(1, N+1):
+    if N%i == 0 :
+       print(i, end=' ')
